f784154e5437ecaac5b96700181297fe7fcd61bb
Ryan F <rfairhur@gmail.com>
2024-02-13 18:56:27 -0800

    Working header

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,15 @@
 <script>
-	export let name;
+	import Header from './Header.svelte';
 </script>
 
+<Header />
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
 </main>
 
 <style type="text/scss">
+
 	main {
 		text-align: center;
 		padding: 1em;
@@ -15,20 +17,8 @@
 		margin: 0 auto;
 	}
 
-	p { 
-		color: black;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  	:global(body) {
+    	background-color: rgb(255, 255, 255);
+    	color: white;
+  	}
 </style>
---
+++ src/Header.svelte
@@ -0,0 +1,20 @@
+<header>
+    <a href="/">Weather</a>
+</header>
+
+<style type="text/scss">
+    header {
+        overflow: hidden;
+        background-color: blue;
+        font-family: Arial;
+        text-align: center;
+        padding: 1rem;
+
+        a {
+            color: white;
+            text-decoration: none;
+            cursor: pointer;
+            font-size: 2rem;
+        }
+    }
+</style>
